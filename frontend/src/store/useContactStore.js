46539945f57cfe1c0723ac3e62cfecc0cbfab8cd
poojaupadhye0976 <pooja.upadhye@pinnacle.in>
2025-06-22 11:34:02 +0530

    feat: add Zustand store and theme variables

--- frontend/src/store/useContactStore.js
+++ frontend/src/store/useContactStore.js
@@ -3,13 +3,18 @@ import { create } from 'zustand';
 export const useContactStore = create((set) => ({
   selectedContactId: null,
   isModalOpen: false,
-  modalMode: 'view', // 'view', 'add', 'edit'
+  modalMode: 'view',
   searchQuery: '',
   showFavoritesOnly: false,
+  currentPage: 1,
   
+  // Actions
   setSelectedContactId: (id) => set({ selectedContactId: id }),
   openModal: (mode) => set({ isModalOpen: true, modalMode: mode }),
   closeModal: () => set({ isModalOpen: false, selectedContactId: null }),
   setSearchQuery: (query) => set({ searchQuery: query }),
   toggleShowFavoritesOnly: () => set((state) => ({ showFavoritesOnly: !state.showFavoritesOnly })),
+  setShowFavoritesOnly: (value) => set({ showFavoritesOnly: value }),
+  setCurrentPage: (page) => set({ currentPage: page }),
+  
 }));
